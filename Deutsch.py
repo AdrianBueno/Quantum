619f4d1a6a2c0b112acc80cb8874e7c96d9aa917
Ian <AdrianBueno@users.noreply.github.com>
2020-04-27 16:03:37 +0200

    Update Deutsch.py

--- Deutsch.py
+++ Deutsch.py
@@ -1,49 +1,45 @@
 from projectq.ops import H, Measure, X, Y, Z, CNOT
 from projectq import MainEngine
 from projectq.meta import Loop, Compute, Uncompute, Control
+from projectq.backends import CircuitDrawer
 
-#Algoritmo de Deutsch básico.
+#Algoritmo de Deutsch.
 def runDeutsch(eng, oracle):
-    # Instanciamos dos qubits
+    drawing_engine = CircuitDrawer() # Esto nos permite que se registre todo el circuito ejecutado.
+    # Instanciamos dos qubits, estos se inicializan con valor |0>
     q1 = eng.allocate_qubit()
     q2 = eng.allocate_qubit()
-    # Mediante las puertas de Pauli fijamos el estado de incicio de los qubits
-    Z | q1 # inicializamos al estado |0> el primer qubit
-    X | q2 # y al estado |1> el segundo
-    # El estado del sistema sería el indicado en la sección 5.1.2
+    X | q2 #Para realizar la inversion de fase dejamos el estado |1> con la puerta X
 
-    # Mediante la puerta de Hadamard pones los dos qubit en estado de superposición (sección 5.1.3)
-    H | q1
-    H | q2
-    # Aplicamos el oráculo, en el circuito el operador U_f, que puede ser una función continua o balanceada
-    oracle(q1,q2)
+    # Ahora dejamos en superposicion los dos qubits mediante la puerta H.
+    H | q1 # 1/sqrt(2) (|0> + |1>)
+    H | q2 # 1/sqrt(2) (|0> - |1>)
 
-    # Como seguimos teniendo una superposición aplicando de nuevo la puerta H,
-    # la superposición se deshará y mediremos 0 o 1 en función de lo que tenga dentro el oráculo.
-    H | q1
-    # Medimos.
+    oracle(q1,q2) # Aplicamos U y obtenemos el estado: -1^f(x) |x> q2
+
+    H | q1 # Aplicamos H para quitar la superposicion y obtener el estado 0|0> o -1|0>
+
+    #Mediciones
     Measure | q1
     Measure | q2
 
-    eng.flush()
-    # imprimimos el resultado
+    eng.flush() #Con esta instruccion finalizamos la parte cuantica
+    # Ahora mostramos el valor de q1, si este esta en el estado |0> el oraculo implementa una funcion constante.
+    # Por otro lado si tiene el estado |1> seria balanceada.
     print("Measured: {}".format(int(q1)))
+    #Con esta instruccion obtendremos como salida el codigo latex que permitira mostrar el circuito ejecutado
+    #print(drawing_engine.get_latex())
 
-#Estos son los posibles oráculos que podemos pasar en este algoritmo
 
 def function_deutsch_balanceada(q1, q2):
-    # Aplicamos la Controlled NOT.
-    CNOT | (q1, q2)
-    # Aplicamos a q2 la pauli X
-    X | q2
-    # Entonces q1 = 1 -> 0 XOR f(x) -> f(1) = 1 y f(0) = 0
+    # Aplicamos la Controlled NOT
+    CNOT | (q1, q2) # q1 es el qubit de control y q2 el objetivo.
+    # Ahora si q1 == 1 entonces q2 inventira su valor.
 
-def function_deutsch_continua(q1, q2):
-    # Aplicamos a q2 la pauli X: X 0 = 1 -> 1 XOR f(x) -> f(1) = 0 y f(0) = 1
-    X | q2
+def function_deutsch_constante(q1, q2):
+    pass # Para la constante no hacemos nada y q2 seguira como esta
 
 
 if __name__ == "__main__":
-
-    eng = MainEngine()  
-    runDeutsch(eng, function_deutsch_balanceada)
+    eng = MainEngine() # Con esta instruccion iniciamos la parte cuantica
+    runDeutsch(eng, function_deutsch_constante) # Pasamos la funcion constante como oraculo.
